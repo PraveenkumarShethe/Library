9d4e17aa8b12fc75170462d4a1ac2dcf2f6f629a
Praveenkumar <praveen.shethe7@gmail.com>
2017-07-13 20:07:09 +0530

    Library-06 #comment added Principal to all rest urls and adding new user also done #time 20mins

--- src/main/java/com/praveen/shethe/controller/BookController.java
+++ src/main/java/com/praveen/shethe/controller/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
+import java.security.Principal;
 
 /**
  * Created by Praveenkumar on 3/7/2017.
@@ -22,21 +23,21 @@ public class BookController {
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     @Transactional(Transactional.TxType.NEVER)
-    public Iterable<Book> getAllBooks() {
+    public Iterable<Book> getAllBooks(Principal principal) {
         return bookRepository.findAll();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     @Transactional(Transactional.TxType.NEVER)
-    public Book getBook(@PathVariable("id") Long id ) {
+    public Book getBook(Principal principal,@PathVariable("id") Long id ) {
         return bookRepository.findOne(id);
     }
 
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     @Transactional(Transactional.TxType.REQUIRES_NEW)
-    public void insertNewBook(@RequestBody Book book){
+    public void insertNewBook(Principal principal,@RequestBody Book book){
         bookRepository.save(book);
     }
 
