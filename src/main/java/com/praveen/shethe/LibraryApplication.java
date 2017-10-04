01907bdf9ab24ec93b2c3e10962d5bac828ce6bb
Praveenkumar <praveen.shethe7@gmail.com>
2017-10-04 19:34:58 +0530

    Library-12 #comment Uploading file(e-book)

--- src/main/java/com/praveen/shethe/LibraryApplication.java
+++ src/main/java/com/praveen/shethe/LibraryApplication.java
@@ -1,12 +1,78 @@
 package com.praveen.shethe;
 
+import org.springframework.beans.factory.groovy.GroovyBeanDefinitionReader;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
+import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
+import org.springframework.core.env.CommandLinePropertySource;
+import org.springframework.core.io.Resource;
+
+
+/**
+ * Classes that can be used to bootstrap and launch a Spring application from a Java main
+ * method. By default class will perform the following steps to bootstrap your
+ * application:
+ * <p>
+ * <ul>
+ * <li>Create an appropriate {@link ApplicationContext} instance (depending on your
+ * classpath)</li>
+ * <li>Register a {@link CommandLinePropertySource} to expose command line arguments as
+ * Spring properties</li>
+ * <li>Refresh the application context, loading all singleton beans</li>
+ * <li>Trigger any {@link CommandLineRunner} beans</li>
+ * </ul>
+ * <p>
+ * <pre class="code">
+ * &#064;Configuration
+ * &#064;EnableAutoConfiguration
+ * public class MyApplication  {
+ * <p>
+ * // ... Bean definitions
+ * <p>
+ * public static void main(String[] args) throws Exception {
+ * SpringApplication.run(MyApplication.class, args);
+ * }
+ * </pre>
+ * <p>
+ * <p>
+ * For more advanced configuration a {@link SpringApplication} instance can be created and
+ * customized before being run:
+ * <p>
+ * <pre class="code">
+ * public static void main(String[] args) throws Exception {
+ * SpringApplication app = new SpringApplication(MyApplication.class);
+ * // ... customize app settings here
+ * app.run(args)
+ * }
+ * </pre>
+ * <p>
+ * {@link SpringApplication}s can read beans from a variety of different sources. It is
+ * generally recommended that a single {@code @Configuration} class is used to bootstrap
+ * your application, however, any of the following sources can also be used:
+ * <p>
+ * <ul>
+ * <li>{@link Class} - A Java class to be loaded by {@link AnnotatedBeanDefinitionReader}
+ * </li>
+ * <li>{@link Resource} - An XML resource to be loaded by {@link XmlBeanDefinitionReader},
+ * or a groovy script to be loaded by {@link GroovyBeanDefinitionReader}</li>
+ * <li>{@link Package} - A Java package to be scanned by
+ * {@link ClassPathBeanDefinitionScanner}</li>
+ * <li>{@link CharSequence} - A class name, resource handle or package name to loaded as
+ * appropriate. If the {@link CharSequence} cannot be resolved to class and does not
+ * resolve to a {@link Resource} that exists it will be considered a {@link Package}.</li>
+ * </ul>
+ *
+ * @author Praveenkumar
+ */
 
 @SpringBootApplication
 public class LibraryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
 }
