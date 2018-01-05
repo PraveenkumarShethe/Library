0566a1314fe5cc1486607248f8587789abfa5de2
Praveenkumar <praveen.shethe7@gmail.com>
2018-01-05 16:14:29 +0530

    Library-10 Documentation

--- src/main/java/com/praveen/shethe/security/StatelessAuthenticationFilter.java
+++ src/main/java/com/praveen/shethe/security/StatelessAuthenticationFilter.java
@@ -51,6 +51,35 @@ public class StatelessAuthenticationFilter extends GenericFilterBean {
 		this.tokenAuthenticationService = taService;
 	}
 
+	/**
+	 * The <code>doFilter</code> method of the Filter is called by the
+	 * container each time a request/response pair is passed through the
+	 * chain due to a client request for a resource at the end of the chain.
+	 * The FilterChain passed in to this method allows the Filter to pass
+	 * on the request and response to the next entity in the chain.
+	 *
+	 * <p>A typical implementation of this method would follow the following
+	 * pattern:
+	 * <ol>
+	 * <li>Examine the request
+	 * <li>Optionally wrap the request object with a custom implementation to
+	 * filter content or headers for input filtering
+	 * <li>Optionally wrap the response object with a custom implementation to
+	 * filter content or headers for output filtering
+	 * <li>
+	 * <ul>
+	 * <li><strong>Either</strong> invoke the next entity in the chain
+	 * using the FilterChain object
+	 * (<code>chain.doFilter()</code>),
+	 * <li><strong>or</strong> not pass on the request/response pair to
+	 * the next entity in the filter chain to
+	 * block the request processing
+	 * </ul>
+	 * <li>Directly set headers on the response after invocation of the
+	 * next entity in the filter chain.
+	 * </ol>
+	 */
+
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
