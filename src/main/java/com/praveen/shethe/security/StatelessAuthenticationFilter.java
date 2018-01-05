360c2d1351fa1079d1afc3cad0b8aa7e45149db7
Praveenkumar <praveen.shethe7@gmail.com>
2018-01-05 16:11:40 +0530

    Library-10 #comment Documentation

--- src/main/java/com/praveen/shethe/security/StatelessAuthenticationFilter.java
+++ src/main/java/com/praveen/shethe/security/StatelessAuthenticationFilter.java
@@ -1,5 +1,9 @@
 package com.praveen.shethe.security;
 
+/**
+ * Created by Praveenkumar on 5/1/2018.
+ * */
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
@@ -10,6 +14,34 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+
+/**
+ * Simple base implementation of {@link javax.servlet.Filter} which treats
+ * its config parameters ({@code init-param} entries within the
+ * {@code filter} tag in {@code web.xml}) as bean properties.
+ *
+ * <p>A handy superclass for any type of filter. Type conversion of config
+ * parameters is automatic, with the corresponding setter method getting
+ * invoked with the converted value. It is also possible for subclasses to
+ * specify required properties. Parameters without matching bean property
+ * setter will simply be ignored.
+ *
+ * <p>This filter leaves actual filtering to subclasses, which have to
+ * implement the {@link javax.servlet.Filter#doFilter} method.
+ *
+ * <p>This generic filter base class has no dependency on the Spring
+ * {@link org.springframework.context.ApplicationContext} concept.
+ * Filters usually don't load their own context but rather access service
+ * beans from the Spring root application context, accessible via the
+ * filter's {@link #getServletContext() ServletContext} (see
+ * {@link org.springframework.web.context.support.WebApplicationContextUtils}).
+ *
+ * @author Juergen Hoeller
+ * @since 06.12.2003
+ * @see #addRequiredProperty
+ * @see #initFilterBean
+ * @see #doFilter
+ */
 
 public class StatelessAuthenticationFilter extends GenericFilterBean {
 
